1e5508ba075fd45e5414cf151a1868185464c4ba
MaybeMIM <2412178358@qq.com>
2023-12-05 23:31:08 +0800

    访客端init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./assets/base.scss";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+// 引入cookies
+import VueCookies from "vue-cookies";
+// 引入element
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+const app = createApp(App);
+
+//图标 图标在附件中
+import "@/assets/icon/iconfont.css";
+
+app.use(router);
+app.use(ElementPlus);
+app.config.globalProperties.VueCookies = VueCookies;
+
+app.mount("#app");
