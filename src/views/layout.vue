db8c3cfa30cdc943be5986248336159c74c23f8a
MaybeMIM <2412178358@qq.com>
2023-12-09 23:42:05 +0800

    导航栏滚动隐藏显示

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
-    <div class="header">
-      <div
-        class="header_content"
-        :style="{ width: proxy.globalInfo.bodyWidth + 'px' }"
-      >
+    <div class="header" v-if="showHeader">
+      <div class="header_content" :style="{ width: proxy.globalInfo.bodyWidth + 'px' }">
         <router-link to="/" class="header_logo">
           <!-- TODO:优化一下 用图标svg -->
-          <span
-            v-for="(item, index) in logoInfo"
-            :style="{ color: item.color }"
-            :key="index"
-            >{{ item.letter }}</span
-          >
+          <span v-for="(item, index) in logoInfo" :style="{ color: item.color }" :key="index">{{ item.letter }}</span>
         </router-link>
         <!-- 模块信息 -->
         <div class="menu-penal"></div>
@@ -33,14 +25,14 @@
         </div>
       </div>
     </div>
-    <div>
+    <div :style="{ height: '1500px' }">
       <router-view />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, getCurrentInstance, nextTick } from "vue";
+import { ref, reactive, getCurrentInstance, nextTick, computed, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
 const { proxy } = getCurrentInstance();
 const router = useRouter();
@@ -60,6 +52,34 @@ const logoInfo = [
     color: "blue",
   },
 ];
+
+const showHeader = ref(true)
+
+// 获取滚动条高度 (用于header的行为)
+const getScrollTop = () => {
+  return document.documentElement.scrollTop || document.body.scrollTop
+
+}
+function initScroll() {
+  let initScrollTop = getScrollTop()
+  let scrollType = 0
+  window.addEventListener('scroll', () => {
+    let currentScrollTop = getScrollTop()
+    if (currentScrollTop > initScrollTop) {
+      // 往下滚动
+      scrollType = 1
+    } else {
+      scrollType = 0
+    }
+    initScrollTop = currentScrollTop
+    if (scrollType === 1 && currentScrollTop > 150) {
+      showHeader.value = false
+    } else {
+      showHeader.value = true
+    }
+  })
+}
+onMounted(() => initScroll())
 </script>
 
 <style lang="scss" scoped>
@@ -68,24 +88,32 @@ const logoInfo = [
   height: 60px;
   position: fixed;
   box-shadow: 0 2px 6px 0 #ddd;
+  z-index: 999;
+  background: #fff;
+  transition: all 0.3s;
+
   &_content {
     display: flex;
     height: 60px;
     align-items: center;
     margin: 0px auto;
   }
+
   &_logo {
     display: block;
     margin-right: 5px;
     text-decoration: none;
     font-size: 30px;
   }
+
   .menu-penal {
     flex: 1;
   }
+
   .user-info-penal {
     display: flex;
     width: 300px;
+
     span {
       margin-left: 3px;
     }
