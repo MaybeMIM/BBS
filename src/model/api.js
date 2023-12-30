86db542287bf7a54804c2ed94e9ed6dc28cd7057
MaybeMIM <2412178358@qq.com>
2023-12-30 20:46:41 +0800

    api接口文件

---
+++ src/model/api.js
@@ -0,0 +1,16 @@
+import Request from '@/utils/request'
+
+// 获取文章列表
+export function loadArticleList(params, showLoading) {
+  return Request({ url: '/forum/loadArticle', params, showLoading })
+}
+
+// 获取用户信息
+export function getUserInfos(params) {
+  return Request({ url: '/getUserInfo', params })
+}
+
+// 获取用户信息
+export function loadBoardList(params) {
+  return Request({ url: '/board/loadBoard', params })
+}
