c7b75eccc045cb2f70db7a2bf4d770b585c54dc6
MaybeMIM <2412178358@qq.com>
2024-01-28 19:42:14 +0800

    feat:发布文章的markdown、富文本编辑器

---
+++ src/components/editor-html.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="editor-html">
+    <Toolbar
+      style="border-bottom: 1px solid #ccc"
+      :editor="editorRef"
+      :defaultConfig="toolbarConfig"
+      :mode="mode"
+    />
+    <Editor
+      :style="{ height: height + 'px', 'overflow-y': 'hidden' }"
+      :modal-value="modalValue"
+      :defaultConfig="editorConfig"
+      :mode="mode"
+      @onCreated="handleCreated"
+      @onChange="onChange"
+    />
+  </div>
+</template>
+
+<script setup>
+import '@wangeditor/editor/dist/css/style.css'
+import { onBeforeUnmount, ref, shallowRef, getCurrentInstance } from 'vue'
+import { Toolbar, Editor } from '@wangeditor/editor-for-vue'
+import message from '@/utils/message.js'
+import store from '@/store'
+
+const { proxy } = getCurrentInstance()
+
+const props = defineProps({
+  modalValue: {
+    type: String,
+    default: ''
+  },
+  height: {
+    type: Number,
+    default: 500
+  }
+})
+const emit = defineEmits()
+
+const mode = ref('default')
+const editorRef = shallowRef()
+
+const toolbarConfig = {
+  // 排除菜单组 即不需要的功能
+  excludeKeys: ['uploadVideo']
+}
+
+const editorConfig = {
+  placeholder: '请输入内容',
+  excludeKeys: ['uploadVideo'],
+  MENU_CONF: {
+    uploadImage: {
+      maxFileSize: 3 * 1024 * 1024,
+      server: 'api/file/uploadImage',
+      fileName: 'file',
+      customInsert (responseData, insertFn) {
+        // 正常请求
+        if (responseData.code === 200) {
+          insertFn(
+            proxy.globalInfo.imageUrl + responseData.data.fileName,
+            '',
+            ''
+          )
+          return
+        } else if (responseData.code === 901) {
+          // 登录超时
+          store.commit('showLogin', true)
+          store.commit('updateLoginUserInfo', null)
+          return
+        }
+        message.success.error(responseData.info)
+      }
+    }
+  }
+}
+
+// 创建 editor 实例
+function handleCreated (editor) {
+  editorRef.value = editor
+}
+
+function onChange (editor) {
+  emit('update:modalValue', editor.getHtml())
+}
+
+// 组件销毁 也要及时销毁编辑器
+onBeforeUnmount(() => {
+  const editor = editorRef.value
+  if (editor == null) return
+  editor.destroy()
+})
+</script>
+
+<style lang="scss" scoped>
+.editor-html {
+  border: 1px solid #ddd;
+}
+</style>
---
+++ src/components/editor-markdown.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-md-editor
+    :modal-value="modalValue"
+    :height="height + 'px'"
+    :disabled-menus="[]"
+    :include-level="[1, 2, 3, 4, 5, 6]"
+    @upload-image="handleUploadImage"
+    @change="change"
+  >
+  </v-md-editor>
+</template>
+
+<script setup>
+import VMdEditor from '@kangc/v-md-editor'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import githubTheme from '@kangc/v-md-editor/lib/theme/github'
+import '@kangc/v-md-editor/lib/theme/style/github.css'
+import hljs from 'highlight.js'
+import { uploadImage } from '@/model/api.js'
+import { getCurrentInstance } from 'vue'
+const { proxy } = getCurrentInstance()
+// 初始化 代码高亮风格
+VMdEditor.use(githubTheme, {
+  Hljs: hljs
+})
+
+const props = defineProps({
+  modalValue: {
+    type: String,
+    default: ''
+  },
+  height: {
+    type: Number,
+    default: 500
+  }
+})
+
+const emit = defineEmits()
+
+function change (markdownContent, htmlContent) {
+  // 实时输入内容  |  解释为html的结构
+  emit('update:modalValue', markdownContent)
+  emit('htmlContent', htmlContent)
+}
+
+// 自定义图片上传
+async function handleUploadImage (event, insertImage, files) {
+  let result = await uploadImage({ file: files[0] })
+
+  if (!result) return
+
+  const url = proxy.globalInfo.imageUrl + result.data.fileName
+  // 在文本编辑器中插入刚上传的图片
+  insertImage({
+    url,
+    desc: '图片'
+  })
+}
+</script>
+
+<style lang="scss" scoped></style>
